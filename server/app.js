5ed60cef11c9f153b1b9fc7f307b5b7bf53c25d7
m-aguirre <mpaguirre@dons.usfca.edu>
2017-05-24 10:44:51 -0700

    heroku comment removed

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
-//comment yay!
+
 const app = express();
 
 app.use(middleware.morgan('dev'));
