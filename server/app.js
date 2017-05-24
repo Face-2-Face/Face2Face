2ad50342789cc674f5a56176578154cb98c1cab3
m-aguirre <mpaguirre@dons.usfca.edu>
2017-05-24 10:46:10 -0700

    heroku comment 2

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
-
+//hello
 const app = express();
 
 app.use(middleware.morgan('dev'));
