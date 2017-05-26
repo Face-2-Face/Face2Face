541b9de1c3666b9911e213588c1b8e180b067570
Yurika Jacobsson <nakiruy@gmail.com>
2017-05-25 19:19:46 -0700

    Changed the redisClient url.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
+// const redisClient = require('redis').createClient();
+const redisClient = require('redis').connect('redis://h:pdc54228c4d9ceb0ac8771a7dab1571cd5d782b695be019cec5569f7e4fa097ae@ec2-34-197-198-120.compute-1.amazonaws.com:31229');
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -12,7 +13,7 @@ module.exports.verify = (req, res, next) => {
 module.exports.session = session({
   store: new RedisStore({
     client: redisClient,
-    host: 'redis://h:pdc54228c4d9ceb0ac8771a7dab1571cd5d782b695be019cec5569f7e4fa097ae@ec2-34-197-198-120.compute-1.amazonaws.com:31229',
+    host: 'localhost',
     port: 6379,
     // url: 'redis://h:pdc54228c4d9ceb0ac8771a7dab1571cd5d782b695be019cec5569f7e4fa097ae@ec2-34-197-198-120.compute-1.amazonaws.com:31229'
   }),
