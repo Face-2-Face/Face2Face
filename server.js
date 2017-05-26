05d78cf73cd41f536db1a5dd2e78efeb5bbdde90
Yurika Jacobsson <nakiruy@gmail.com>
2017-05-25 17:00:18 -0700

    Added some changes on server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+// const middleware = require('./middleware');
+// const routes = require('./routes');
+
+const app = express();
+// const app = require('./app');
+// const db = require('./db');
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log('Better be listening on port ' + port);
+});
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello World PLZ')
+})
