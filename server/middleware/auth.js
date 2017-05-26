e70fb9f3de36d6884136855f9b0a03160080a20b
Yurika Jacobsson <nakiruy@gmail.com>
2017-05-25 19:10:21 -0700

    Changed the host from localhost to redis.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -12,9 +12,9 @@ module.exports.verify = (req, res, next) => {
 module.exports.session = session({
   store: new RedisStore({
     client: redisClient,
-    host: 'localhost',
+    host: 'redis://h:pdc54228c4d9ceb0ac8771a7dab1571cd5d782b695be019cec5569f7e4fa097ae@ec2-34-197-198-120.compute-1.amazonaws.com:31229',
     port: 6379,
-    url: 'redis://h:pdc54228c4d9ceb0ac8771a7dab1571cd5d782b695be019cec5569f7e4fa097ae@ec2-34-197-198-120.compute-1.amazonaws.com:31229'
+    // url: 'redis://h:pdc54228c4d9ceb0ac8771a7dab1571cd5d782b695be019cec5569f7e4fa097ae@ec2-34-197-198-120.compute-1.amazonaws.com:31229'
   }),
   secret: 'more laughter, more love, more life',
   resave: false,
