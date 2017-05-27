ff7c84f775dd603b0882dbc81630f1de147d2223
Yurika Jacobsson <nakiruy@gmail.com>
2017-05-26 17:21:42 -0700

    Navigating the localhost navigates to something other than login now.

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render () {
+        return (
+            <div>zak the angel</div>
+        );
+    }
+}
+
+export default Main;
