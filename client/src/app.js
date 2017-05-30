8a92118a98e70b7d822fec4aae2ae279290eeda5
jen1425 <jenniferhwa@gmail.com>
2017-05-30 13:52:33 -0700

    Set up routes with react-router

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import Home from './components/Home.js';
 
-ReactDOM.render(<h1>Hello World from React!!</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+    </div>
+  </Router>, document.getElementById('root'));
---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is the Home component</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
