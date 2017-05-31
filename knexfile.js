407264e93f7514ccae105a95d5b0b5a1d082c190
jen1425 <jenniferhwa@gmail.com>
2017-05-31 11:09:34 -0700

    Update knexfile.js process.env.DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,16 @@
-const config = require('config');
-
-module.exports = config['knex'];
+module.exports = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  pool: {
+      "min": 2,
+      "max": 10
+    },
+    migrations: {
+      "tableName": "knex_migrations",
+      "directory": "db/migrations"
+    },
+    seeds: {
+      "directory": "db/seeds"
+    },
+    debug: false
+};
