d26acd4be91ce0883f96146c4258f924c0399f8d
jen1425 <jenniferhwa@gmail.com>
2017-05-31 16:56:18 -0700

    Move order to keep server routes

--- server/app.js
+++ server/app.js
@@ -22,10 +22,9 @@ app.use(middleware.flash());
 app.use('/public', express.static(path.join(__dirname, '../public')));
 
 app.use('/', routes.auth);
-//allows us to use React Router
-app.use('*', routes.auth);
-
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
+//allows us to use React Router
+app.use('*', routes.auth);
 
 module.exports = app;
