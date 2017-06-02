b7323826d517869f00eb904628d219277a6204cb
jen1425 <jenniferhwa@gmail.com>
2017-06-02 13:48:33 -0700

    Load user profile info into Home and display it in Profile

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import Header from './Header.jsx';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      profile: {}
+    }
+  }
+
+  componentDidMount() {
+  let that = this;
+  axios.get('/api/user')
+    .then(function(response) {
+      that.setState({profile: response.data});
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   }
 
   render() {
+    console.log('HOME', this.state.profile)
     return (
       <div>
         <Header />
+        <Link to={{pathname:'/profile', state: {profile: this.state.profile}}}>Profile</Link>
+        <div><h2>READY TO VIDEO CHAT</h2></div>
       </div>
     )
   }
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import Header from './Header.jsx';
 
+
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      profile: this.props.location.state.profile
+    }
   }
 
+
+
+
   render() {
+    let profile = this.state.profile;
+
     return (
       <div>
         <Header />
-        <h3>This is the Profile Page</h3>
-        <h3>Hello</h3>
+        <h2 className="test">Hello {profile.first}</h2>
+        <img className="img-circle profile-img"src={profile.photo}/>
       </div>
     )
   }
