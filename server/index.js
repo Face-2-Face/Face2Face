2bde39038d5be85799b17ad85ce9ae7f73abb1b6
Eric Davis <efdavis1@gmail.com>
2017-06-02 16:21:24 -0700

    refactoring twilio video with react components

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const app = require('./app');
 const db = require('../db');
 const port = process.env.PORT || 3000;
 
-
-// app.listen(port, () => {
-//   console.log('Example app listening on port 3000!');
-// });
+app.listen(port, () => {
+  console.log('Example app listening on port 3000!');
+});
