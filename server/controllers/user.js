4ed8d3409070d268929cca204a026c6cf8777841
m-aguirre <mpaguirre@dons.usfca.edu>
2017-06-02 19:52:16 -0700

    route changes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,5 +3,4 @@ const models = require('../../db/models');
 module.exports.getUser = (req, res) => {
   console.log('hey I am here ', req.user);
   res.status(200).send(req.user);
-  //res.send('hey');
 }
