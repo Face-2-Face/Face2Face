e3f50272bf60aeecc29cb45c967c3d2f8105fc60
Eric Davis <efdavis1@gmail.com>
2017-06-06 11:16:19 -0700

    switches video index file to our original video file

--- server/indexold.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const app = require('./app');
-const db = require('../db');
-const port = process.env.PORT || 3000;
-
-
-// app.listen(port, () => {
-//   console.log('Example app listening on port 3000!');
-// });
