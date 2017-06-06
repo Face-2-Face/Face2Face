8c19468042bb44fe7ae310a7a90420fc4763abc5
m-aguirre <mpaguirre@dons.usfca.edu>
2017-06-06 11:30:05 -0700

    Renders profile name and image

--- client/src/components/Match.jsx
+++ client/src/components/Match.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 class Match extends React.Component {
   constructor(props){
     super(props)
 
+    this.state = {
+      profile: {
+        first: '',
+        photo: ''
+      }
+    }
+  }
+
+  componentDidMount() {
+    let that = this;
+    const id = this.props.data.match;
+    axios.get('/api/profiles/' + id )
+    .then(function(response) {
+      console.log('server communication from match', response.data);
+      that.setState({profile: response.data});
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div className="match">
+        <img className="img-circle center-block match-img"src={this.state.profile.photo}/>
         {console.log('matchy', this.props.data)}
-        <h1>{this.props.data.match}</h1>
+        <p>{this.state.profile.first}</p>
       </div>
     )
   }
