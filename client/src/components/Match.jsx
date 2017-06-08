0f384b63f3a63bcd726e85aa1f610dc12dbd676e
Yurika Jacobsson <nakiruy@gmail.com>
2017-06-08 14:30:10 -0700

    Adding the unique chat room for client

--- client/src/components/Match.jsx
+++ client/src/components/Match.jsx
@@ -1,14 +1,10 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
 import ChatRoom from './ChatRoom.jsx'
-
 class Match extends React.Component {
   constructor(props){
     super(props)
-
     this.state = {
       userProfile: {},
       matchProfile: {
@@ -16,8 +12,8 @@ class Match extends React.Component {
         photo: ''
       }
     }
+    this.makePath = this.makePath.bind(this);
   }
-
   componentDidMount() {
     let that = this;
     const id = this.props.matchID.match;
@@ -29,12 +25,23 @@ class Match extends React.Component {
       console.log(error);
     });
   }
-
+  makePath() {
+    if(this.state.matchProfile.id < this.props.userProfile.id) {
+      var path = "/chat/" + this.state.matchProfile.id + "z" + this.props.userProfile.id;
+    } else {
+      var path = "/chat/" + this.props.userProfile.id + "z" + this.state.matchProfile.id;
+    }
+    return path;
+  }
   render() {
+    var path = "/chat/";
+    if(this.state.matchProfile.id) {
+      path = this.makePath();
+    }
     return (
       <div className="match" >
         <img className="img-circle center-block match-img"src={this.state.matchProfile.photo}/>
-        <Link to={{pathname: '/chat', state: {userProfile: this.props.userProfile, matchProfile: this.state.matchProfile}}}>
+        <Link to={{pathname: path, state: {userProfile: this.props.userProfile, matchProfile: this.state.matchProfile}}}>
           <p>{this.state.matchProfile.first}</p>
           <p>click to enter chat...</p>
         </Link>
@@ -42,5 +49,4 @@ class Match extends React.Component {
     )
   }
 }
-
 export default Match;
