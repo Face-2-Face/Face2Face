acf4711e0c4fb06b67de2dba1fd3517ea687a0b2
jen1425 <jenniferhwa@gmail.com>
2017-06-12 15:02:21 -0700

    Add matching logic to determine user video chat assignment ( still in progress )

--- server/controllers/lobby.js
+++ server/controllers/lobby.js
@@ -21,73 +21,41 @@ module.exports.addToLobby = (req, res) => {
     });
 };
 
-
 module.exports.getPersonToChat = (req, res) => {
-  models.Profile.query(function(qb) {
-//  qb.where('gender', '=', req.user.prefGender)
-  qb.where('room', '=', 'lobby')
-   .andWhere('id', '<>', req.user.id)
-  // .andWhere('age_min', '>', req.user.prefAge_min)
-//  .andWhere('age_max', '<', req.user.preAge_max)
-  }).fetchAll()
-  .then(function(model) {
-    // pick one matching model
-    var index = randomlySelectProfile(model.models.length);
-    console.log('MATCHING MODEL', model.models[index]);
-    res.status(200).send(model.models[index]);
-  })
-  .catch(() => {
-    res.sendStatus(404);
-  });
-};
-
-<<<<<<< HEAD
-||||||| merged common ancestors
-  // Get your profile and Check if you already have a room assignment
-  // 
   models.Profile.where({ id: req.user.id}).fetch()
     .then(profile => {
         let result;
         if (!profile) {
         throw profile;
       }
-      console.log('ROOM in MODEL', profile.get("room"));
-      if (profile.get("room") !== 'lobby' && profile.get("room") !== 'false') {
-        result = {room: profile.get("room"), otherID: null}
-        res.status(200).send(result);
-      } else {
-        // do DB query to try to find a match
-        result = {room: 'lobby', otherID: null}
-        res.status(200).send(result);
-      }
-    })
-    .error(err => {
-      res.status(500).send(err);
-    })
-    .catch(() => {
-      res.sendStatus(404);
-    });
-=======
-  // Get your profile and Check if you already have a room assignment
-  // 
-  models.Profile.where({ id: req.user.id}).fetch()
-    .then(profile => {
-        let result;
-        if (!profile) {
-        throw profile;
-      }
-      console.log('ROOM in MODEL', profile.get("room"));
+      console.log('lobby.js User1/2 Room -----> ', profile.get("room"));
       if (profile.get("room") !== 'lobby') {
         result = {room: profile.get("room"), otherID: null}
         res.status(200).send(result);
       } else {
-        // do DB query to try to find a match
-        if (profile.get("id") === 12) {
-          result = {room: '12-13', otherID: 13};
-        } else {
-          result = {room: 'lobby', otherID: null};
-        }
-        res.status(200).send(result);
+          models.Profile.query(function(qb) {
+            //  qb.where('gender', '=', req.user.prefGender)
+              qb.where('room', '=', 'lobby')
+              .andWhere('id', '<>', req.user.id)
+              .andWhere('age_min', '>', req.user.prefAge_min)
+             .andWhere('age_max', '<', req.user.prefAge_max)
+              }).fetchAll()
+              .then(function(model) {
+                // pick one matching model
+                if (model.length > 0) {
+                  var index = randomlySelectProfile(model.models.length);
+                  var randomProfile = model.models[index];
+                  var room = profile.get("id") + '-' + randomProfile.get("id");
+                  console.log('lobby.js User1 ----->', randomProfile, room);
+                  result = {room: room, otherID: randomProfile.get("id")};
+                } else {
+                  result = {room: 'lobby', otherID: null};          
+                }
+                res.status(200).send(result);
+              })
+              .catch(() => {
+                res.sendStatus(404);
+              });
       }
     })
     .error(err => {
@@ -96,25 +64,8 @@ module.exports.getPersonToChat = (req, res) => {
     .catch(() => {
       res.sendStatus(404);
     });
->>>>>>> Assign user to a video chat room who is the first user
-
-  models.Profile.query(function(qb) {
-  qb.where('gender', '=', req.user.prefGender)
-  .andWhere('id', '<>', req.user.id)
-  .andWhere('age_min', '>', req.user.prefAge_min)
-  .andWhere('age_max', '<', req.user.preAge_max)
-  }).fetch()
-  .then(function(model) {
-    // pick one matching model 
-    console.log('MATCHING MODEL', model);
-    res.status(200).send(model);
-  })
-  .catch(() => {
-    res.sendStatus(404);
-  });
+};
 
 randomlySelectProfile = (max) => {
   return Math.floor(Math.random() * (max));
 }
-
-};
