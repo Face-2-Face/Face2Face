b778df4131a2dfbedb7192ec18e05710b0be1e80
nakiruy <nakiruy@gmail.com>
2017-06-12 17:22:59 -0700

    Revert "Jun12"

--- client/src/app.js
+++ client/src/app.js
@@ -14,20 +14,24 @@ import Settings from './components/PreferencesForm';
 import Lobby from './components/Lobby';
 import ChatRoom from './components/ChatRoom';
 import MatchList from './components/MatchList';
-import PublicProfile from './components/PublicProfile';
-
+import VideoChatRoom from './components/VideoChatRoom';
+import Logout from './components/Logout';
+import PostChat from './components/PostChat';
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path="/" component={Home} />
       <Route path="/profile" component={Profile} />
-      <Route path="/public-profile" component={PublicProfile} />
       <Route path="/settings" component={Settings} />
       <Route path="/lobby" component={Lobby} />
       <Route path="/chat" component={ChatRoom} />
-      <Route path="/chat/:id" component={ChatRoom} />
       <Route path="/matches" component={MatchList} />
+      <Route path="/video" component={VideoChatRoom} />
+      <Route path="/postchat" component={PostChat} />
+      <Route path="/chat/:id" component={ChatRoom} />
       <Route path="/logout" component={Logout} />
+
+
     </div>
   </Router>, document.getElementById('root'));
