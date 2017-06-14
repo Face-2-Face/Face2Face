a2584c3825d067c4ebfb1b44e233dfa2f375a4a4
Yurika Jacobsson <nakiruy@gmail.com>
2017-06-13 18:47:42 -0700

    Edited migration files

--- db/migrations/20170613164934_add_id.js
+++ db/migrations/20170613164934_add_id.js
@@ -1,14 +1,6 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('conversations', function(table) {
-      table.dropColumn('conversation_id');
-      table.increments('id').unsigned().primary();
-    }),
-    knex.schema.table('messages', function(table) {
-      table.increments('id').unsigned().primary();
-      table.integer('conversation').references('conversations.id').onDelete('CASCADE');
-    }),
     knex.schema.table('matches', function(table) {
       table.boolean('temp').defaultTo(false);
     })
@@ -17,13 +9,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('conversations', function(table) {
-      table.dropColumn('id');
-    }),
-    knex.schema.table('messages', function(table) {
-      table.dropColumn('id');
-      table.dropColumn('conversation');
-    }),
     knex.schema.table('matches', function(table) {
       table.dropColumn('temp');
     })
