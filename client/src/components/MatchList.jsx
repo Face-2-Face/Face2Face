b623687a87d13704453e115297a0344bd30282d0
m-aguirre <mpaguirre@dons.usfca.edu>
2017-06-14 10:47:33 -0700

    MatchList refactor

--- client/src/components/MatchList.jsx
+++ client/src/components/MatchList.jsx
@@ -17,22 +17,13 @@ class MatchList extends React.Component {
       matchProfile: {},
       userProfile: this.props.location.state.profile
     }
-    this.enterChat = this.enterChat.bind(this);
-  }
-
-  enterChat(targetUser){
-    console.log('enter chat between', this.props.profile, ' and ', targetUser);
-    this.setState({showChat: true, matchProfile: targetUser})
-
   }
 
   componentDidMount() {
-    console.log('MatchList User Id: ', this.state.userProfile);
     const id = this.state.userProfile.id;
     let that = this;
     axios.get('/api/matches/' + id)
       .then(function(response) {
-        console.log('server communication', response.data);
         that.setState({matchList: response.data});
       })
       .catch(function(error) {
@@ -45,30 +36,15 @@ class MatchList extends React.Component {
       <div>
         <Header />
         <div>
-          {this.state.showChat ?
-
-            <div>
-              <h1>Chat</h1>
-              <ChatRoom userProfile={this.state.userProfile} matchProfile={this.state.matchProfile}/>
-            </div>
-            :
-        <div>
-        <h1>Matches</h1>
-        <div >
-          {this.state.matchList.map(item =>
-            <Match matchID={item} enterChat={this.enterChat} userProfile={this.state.userProfile}/>)}
+          <h1>Matches</h1>
+          <div>
+            {this.state.matchList.map(item =>
+            <Match matchID={item} userProfile={this.state.userProfile}/>)}
+          </div>
         </div>
-        <button onClick={this.enterChat}>Chat</button>
-
-
-        </div>
-      }
-      </div>
     </div>
     )
   }
 }
-
-
 
 export default MatchList;
